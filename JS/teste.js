55a45add8c65281d02038af1d460465886979cf7
Telanium <115567535+Matheus-M-Braga@users.noreply.github.com>
2023-04-02 15:10:23 -0300

    Initial commit

---
+++ JS/teste.js
@@ -0,0 +1,14 @@
+function abrirModal(carregarModal){
+    var modal=document.getElementById(carregarModal)
+    modal.style.display="block"
+    
+}
+
+function fecharModal(fecharModal){
+    var modal=document.getElementById(fecharModal)
+    modal.style.display="none"
+}
+
+/*function ir(){
+    alert('skskksksks')
+}*/
